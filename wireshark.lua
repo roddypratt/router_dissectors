4e7874f62300dd53f2db78cf215b4591a1571cd2
Roddy Pratt <roddy@rascular.com>
2021-05-21 09:57:43 +0100

    Wireshark.lua contains dummy wireshark globals for vscode checking

---
+++ wireshark.lua
@@ -0,0 +1,16 @@
+-- Base Wireshark proto definitions 
+DESEGMENT_ONE_MORE_SEGMENT = 1
+
+Proto = {}
+ProtoField = {}
+ProtoExpert = {}
+base = {}
+
+DissectorTable = {}
+
+--- Returns DissectorTable
+--- @param name string Name of table
+--- @return table
+function DissectorTable.get(name) end
+
+expert = {}
